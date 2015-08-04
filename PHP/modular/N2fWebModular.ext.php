752780a62bd9808b4c1a7d6167c1702328de5f4f
Andrew Male <amale@zibings.com>
2015-08-04 18:13:21 -0400

    [PHP] Adding extension and config files for modular

---
+++ PHP/modular/N2fWebModular.ext.php
@@ -0,0 +1,49 @@
+<?php
+
+	class N2fWebModular extends N2f\ExtensionBase {
+		public function Initialize(N2f\N2f &$N2f) {
+			$N2f->LinkConfigNode(new N2fWebLegacyConfigNode());
+			$N2f->LinkExecuteNode(new N2fWebLegacyExecuteNode());
+			$N2f->LinkGenerationNode(new N2fWebLegacyGenerateNode());
+
+			return;
+		}
+	}
+
+	class N2fWebModularConfigNode extends N2f\NodeBase {
+		public function __construct() {
+			$this->SetKey('N2fWebModularConfigNode')->SetVersion('1.0');
+
+			return;
+		}
+
+		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
+			return;
+		}
+	}
+
+	class N2fWebModularExecuteNode extends N2f\NodeBase {
+		public function __construct() {
+			$this->SetKey('N2fWebModularExecuteNode')->SetVersion('1.0');
+
+			return;
+		}
+
+		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
+			return;
+		}
+	}
+
+	class N2fWebModularGenerateNode extends N2f\NodeBase {
+		public function __construct() {
+			$this->SetKey('N2fWebModularGenerateNode')->SetVersion('1.0');
+
+			return;
+		}
+
+		public function Process($Sender, N2f\DispatchBase &$Dispatch) {
+			return;
+		}
+	}
+
+?>
